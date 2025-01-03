672c235a77a1b51182f1339713c02e1c79add88c
MD Rifat hossain <106229451+withrifat@users.noreply.github.com>
2025-01-03 09:49:58 +0600

    add fix existing bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
+      backgroundImage:{
+        'bg-img': "url('/img/bg.jpg')",
+      },
       fontFamily: {
         sans: ['"Plus Jakarta Sans"', 'sans-serif'],
         inter: ['"Inter"', 'serif'], // Added Inter font here
